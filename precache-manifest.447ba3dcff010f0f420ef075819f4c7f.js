25f83b940ebcc77f49e4f0fdde0ba09a5d88fb49
Lukeveille <luke@lukeleveille.com>
2019-12-03 17:19:59 -0500

    added scrolling buttons to the pic modal

---
+++ precache-manifest.447ba3dcff010f0f420ef075819f4c7f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bccb6b97dd55fb413cca668a9243792",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0a973e06d6de60f3441e",
+    "url": "./static/css/main.79d69349.chunk.css"
+  },
+  {
+    "revision": "c39e63c74618ea0e7f7f",
+    "url": "./static/js/2.a7d0c42f.chunk.js"
+  },
+  {
+    "revision": "0a973e06d6de60f3441e",
+    "url": "./static/js/main.39e05979.chunk.js"
+  },
+  {
+    "revision": "179ab0b15480efda1c4b",
+    "url": "./static/js/runtime-main.849705b4.js"
+  },
+  {
+    "revision": "885c65632679b1d4541044a731c9f54b",
+    "url": "./static/media/7F23768D-4D91-4287-A78A-C6DA6F9D01A9.885c6563.JPG"
+  },
+  {
+    "revision": "fb0d93a6ade26b6d88facb35fbaa6fb2",
+    "url": "./static/media/BoatYK.fb0d93a6.jpg"
+  },
+  {
+    "revision": "9219ea52db2049a3a792ab185105c5ab",
+    "url": "./static/media/YK-in-Cali.9219ea52.png"
+  },
+  {
+    "revision": "a7c133c910eb73f0caa239482007a9ab",
+    "url": "./static/media/armando.a7c133c9.jpg"
+  },
+  {
+    "revision": "f53a48961925f40670b0faff4336c8d1",
+    "url": "./static/media/fito.f53a4896.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "./static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "./static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "./static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "./static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "./static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e7d8a2cf8a085107a0f4c8e3013c3fba",
+    "url": "./static/media/jimmi.e7d8a2cf.jpg"
+  },
+  {
+    "revision": "e2b59cc0695685b11c01ea7053f77da2",
+    "url": "./static/media/unnamed.e2b59cc0.jpg"
+  },
+  {
+    "revision": "771e4e6a5ac2df20b7d39eae3e1cb20c",
+    "url": "./static/media/yatklvb-promo.771e4e6a.mp4"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ac84019ff559f509cd6f755e838d0c54.js"
+  "./precache-manifest.447ba3dcff010f0f420ef075819f4c7f.js"
 );
 
 self.addEventListener('message', (event) => {
